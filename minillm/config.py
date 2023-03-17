052b0021145df4a362d36566cdb6df54c2da2436
Volodymyr Kuleshov <vol.kuleshov@gmail.com>
2023-03-17 13:05:15 -0400

    add 65b parameter model config

--- minillm/config.py
+++ minillm/config.py
@@ -1,12 +1,17 @@
 import torch
 from minillm.llms.opt.config import OPT7B4BitConfig
 from minillm.llms.llama.config import (
-    LLama7B4BitConfig, LLama13B4BitConfig, LLama30B4BitConfig
+    LLama7B4BitConfig, 
+    LLama13B4BitConfig, 
+    LLama30B4BitConfig, 
+    LLama65B4BitConfig
 )
 
 # define some constants
 DEV = torch.device('cuda:0')
-LLAMA_MODELS = ["llama-7b-4bit", "llama-13b-4bit", "llama-30b-4bit"]
+LLAMA_MODELS = [
+    "llama-7b-4bit", "llama-13b-4bit", "llama-30b-4bit", "llama-65b-4bit"
+]
 OPT_MODELS  = ["opt-6.7b-4bit"]
 LLM_MODELS = LLAMA_MODELS + OPT_MODELS
 
@@ -18,6 +23,8 @@ def get_llm_config(model):
         return LLama13B4BitConfig
     elif model == "llama-30b-4bit":
         return LLama30B4BitConfig
+    elif model == "llama-65b-4bit":
+        return LLama65B4BitConfig
     elif model == "opt-6.7b-4bit":
         return OPT7B4BitConfig      
     else:
--- minillm/llms/llama/config.py
+++ minillm/llms/llama/config.py
@@ -13,3 +13,8 @@ class LLama30B4BitConfig:
     hf_config_name = "decapoda-research/llama-30b-hf"
     weights_url = None
     bits = 4
+
+class LLama65B4BitConfig:
+    hf_config_name = "decapoda-research/llama-65b-hf"
+    weights_url = None
+    bits = 4    
